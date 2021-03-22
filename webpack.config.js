a7a4791934542d55fb3c9ca92b1b180b527b5691
Didier <dyder1493@gmail.com>
2021-03-22 12:53:38 +0100

    setup dev dependencies and dist

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (env) => { // transform en fonction car accées paramètre
     return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public' , 'dist'),
             filename: 'bundle.js'
         },
         module: {
@@ -43,7 +43,8 @@ module.exports = (env) => { // transform en fonction car accées paramètre
         devtool: isProduction ? 'source-map' : 'inline-source-map', // permet de renvoyer précisement à la source si erreur
             devServer: {
         contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true
+            historyApiFallback: true,
+            publicPath: '/dist/'
     }
 }
 }
