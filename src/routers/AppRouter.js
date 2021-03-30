33f7b28f2a67f2bf2f86b93bba4a906a411268d7
Didier <dyder1493@gmail.com>
2021-03-30 13:34:52 +0200

    setup publi route dashboard redirect

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -5,10 +5,10 @@ import DashboardPage from '../components/dashboard';
 import AddExpensePage from '../components/addExpense';
 import EditExpensePage from '../components/editExpense';
 import LoginPage from '../components/loginPage';
-import HelpPage from '../components/helpPage';
 import NotFoundPage from '../components/notFoundPage';
 import createHistory from 'history/createBrowserHistory';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createHistory();
 
@@ -16,11 +16,10 @@ const AppRouter = () => (
     <Router history={history}>
         <div>
             <Switch>
-                <Route path="/" component={LoginPage} exact={true} />
+                <PublicRoute path="/" component={LoginPage} exact={true} />
                 <PrivateRoute path="/dashboard" component={DashboardPage} exact={true} />
                 <PrivateRoute path="/create" component={AddExpensePage} />
                 <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-                <Route path="/help" component={HelpPage} />
                 <Route component={NotFoundPage} />
             </Switch>
         </div>
--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -3,21 +3,17 @@ import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 import Header from '../components/header';
 
-/* Route privée permettant de rediriger comme on le souhaite, ici en fonction de logged ou non */
-export const PrivateRoute = ({
+/* Route public permettant de rediriger comme on le souhaite, ici en fonction de logged ou non */
+export const PublicRoute = ({
     isAuthenticated,
     component: Component/* destructure le component passer dans la route de base */,
     ...rest /* spread operator pour récupérer toute ce qui reste comme paramètre dans rest */
 }) => (
     <Route {...rest} component={(props) => (
-        isAuthenticated ? (
-            <div>
-                <Header />
-                <Component {...props} />
-            </div>
-
-        ) : (
-            <Redirect to="/" />
+        isAuthenticated ? ( /* redirect to dashboard if logged */
+            <Redirect to="/dashboard" />
+        ) : ( /* displayu login page if not logged */
+                <Component {...props} /> 
         )
     )} />
 );
@@ -26,4 +22,4 @@ const mapStateToProps = (state) => ({
     isAuthenticated: !!state.auth.uid
 });
 
-export default connect(mapStateToProps)(PrivateRoute);
+export default connect(mapStateToProps)(PublicRoute);
